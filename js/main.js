3b5ad4f22dd40bef9f1bc7fe2e16f717d50ac717
boom7l <13868323606@163.com>
2018-05-22 14:33:31 +0800

    update remember_me

--- js/main.js
+++ js/main.js
@@ -5,7 +5,21 @@ $(function () {
         datatype:'json',
         success:function (data) {
             if(data.code==1000){
-                window.location.href='login.html'
+                if ($.cookie('login_id') != null) {
+                    $.ajax({
+                        url: '/api/accounts/login',
+                        type: 'post',
+                        data: {user:{
+                            login: $.cookie('login_id'),
+                            password:$.cookie('pwd')
+                        }},
+                        success: function (data) {
+                            if (data.code != 0) {
+                                window.location.href='login.html'
+                            }
+                        }
+                    })
+                }
             }
             console.log(data)
         }
